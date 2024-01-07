f8b026953fb27d2047c1065ffdd22876f2684fa2
topolya <topolya@syneforge.com>
2024-01-07 17:25:47 +0200

    The locations section was implemented

---
+++ src/js/accordion.js
@@ -0,0 +1,46 @@
+const characteristicsListElem = document.querySelector('.accordion');
+const characteristicsItemElems = document.querySelectorAll('.accordion__item');
+
+characteristicsItemElems.forEach(elem => {
+  if (elem.children[1].classList.contains('active')) {
+    elem.children[1].style.height = elem.children[1].scrollHeight + 'px';
+  }
+})
+
+const open = (button, dropDown) => {
+  closeAllDrops(button, dropDown);
+  dropDown.style.height = dropDown.scrollHeight + 'px';
+  button.classList.add('active');
+  dropDown.classList.add('active');
+};
+
+const close = (button, dropDown) => {
+  button.classList.remove('active');
+  dropDown.classList.remove('active');
+  dropDown.style.height = '';
+};
+
+const closeAllDrops = (button, dropDown) => {
+  characteristicsItemElems.forEach((elem) => {
+    if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+      close(elem.children[0], elem.children[1]);
+    }
+  })
+}
+
+characteristicsListElem.addEventListener('click', (event) => {
+  const target = event.target;
+  const head = target.closest('.accordion__head');
+
+  if (!head) {
+    return;
+  }
+
+  if (head.classList.contains('accordion__head')) {
+    const parent = target.closest('.accordion__item');
+    const description = parent.querySelector('.accordion__content');
+    description.classList.contains('active') ?
+    close(target, description) :
+    open(head, description);
+  } 
+});
