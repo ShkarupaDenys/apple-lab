0c1f8492b4506c7304b1b9ce0fbe1e539a86440f
topolya <topolya@syneforge.com>
2024-02-19 12:06:15 +0200

    The contacts in header was fixed

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,28 @@
 const header = document.querySelector('.header');
 const navItems = document.querySelectorAll('.navigation__item');
+const contactsToggle = document.querySelector('.header__contacts-link-toggle');
 
 const handleHeader = () => {
-    if (window.scrollY > 40) {
-        header.classList.add('header--fixed');
-    } else {
-        header.classList.remove('header--fixed');
-    }
-}
+  if (window.scrollY > 40) {
+    header.classList.add('header--fixed');
+  } else {
+    header.classList.remove('header--fixed');
+  }
+};
 
-addEventListener("scroll", handleHeader);
+addEventListener('scroll', handleHeader);
 
-navItems.forEach(item => item.addEventListener('mouseover', () => {
-    const active = [...navItems].find(item => item.classList.contains('active'));
+navItems.forEach((item) =>
+  item.addEventListener('mouseover', () => {
+    const active = [...navItems].find((item) =>
+      item.classList.contains('active')
+    );
 
     active.classList.remove('active');
     item.classList.add('active');
-}))
+  })
+);
+
+contactsToggle.addEventListener('click', () => {
+  contactsToggle.classList.toggle('active');
+});
